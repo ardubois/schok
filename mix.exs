d1dc9776086b14f19dffcda24dca965f465d0f86
Andre Rauber Du Bois <rauberdubois@gmail.com>
2026-01-05 18:04:33 -0300

    final compilation step.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Hok.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      compilers: Mix.compilers(), #++ [:my_task],
+      compilers:  [:my_task] ++ Mix.compilers(),
       deps: deps()
     ]
   end
