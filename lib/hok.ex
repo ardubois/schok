4c6e1b8958a583fc8148fba1e5a9201b636a90e1
Andre Rauber Du Bois <rauberdubois@gmail.com>
2026-01-07 12:40:49 -0300

    anonymous functions

--- lib/hok.ex
+++ lib/hok.ex
@@ -54,7 +54,7 @@ defmodule Hok do
 
   defmacro gpufor({:<-,_, [var1, {:..,_, [_b1, e1]}]}, {:<-,_, [var2, {:..,_, [_b2, e2]}]},arr1,arr2, par3, do: body) do
        r=      quote do: MM.comp2xy2D1p(unquote(arr1), unquote(arr2), unquote(par3), unquote(e1), unquote(e2),
-                                          Hok.hok (fn (unquote(arr1),
+                                          Hok.hok_rts (fn (unquote(arr1),
                                                        unquote(arr2),
                                                        unquote(par3),
                                                        unquote(var1),
