5453e1d64e37fe93704f43c0a5efdd52d984da39
Andre Rauber Du Bois <rauberdubois@gmail.com>
2026-01-08 11:13:27 -0300

    anonymous functions

--- lib/hok/cuda_backend.ex
+++ lib/hok/cuda_backend.ex
@@ -606,7 +606,7 @@ def gen_cuda_jit(body,types,param_vars,module,subs) do
    # raise "hell"
   # IO.puts "gen_cuda"
   # IO.inspect param_vars
-  IO.puts subs
+  IO.inspect subs
     pid = spawn_link(fn -> types_server(param_vars,types,module,subs) end)
     Process.register(pid, :types_server)
     code = gen_body(body)
