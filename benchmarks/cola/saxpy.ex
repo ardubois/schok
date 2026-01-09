ac44b5e1b95ea15c0b090972c4a68e41cb05a681
Andre Rauber Du Bois <rauberdubois@gmail.com>
2026-01-09 18:15:20 -0300

    teste anonymous functions

--- benchmarks/cola/saxpy.ex
+++ benchmarks/cola/saxpy.ex
@@ -1,6 +1,6 @@
 require Hok
 
-Hok.defmodule PMap2 do
+Hok.defmodule_rts PMap2 do
 deft saxpy a ~> a ~> a
 defd saxpy(a,b)do
     return 2*a+b
@@ -20,11 +20,10 @@ defd saxpy(a,b)do
       threadsPerBlock = 256;
       numberOfBlocks = 1024;
      # numberOfBlocks = div(size + threadsPerBlock - 1, threadsPerBlock)
-      Hok.spawn(Hok.lt(&PMap2.map_2kernel/5),{numberOfBlocks,1,1},{threadsPerBlock,1,1},[t1,t2,t3,size,func])
+      Hok.spawn_rts(&PMap2.map_2kernel/5,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[t1,t2,t3,size,func])
   end
 end
 
-Hok.include [PMap2]
 
 [arg] = System.argv()
 
@@ -34,30 +33,47 @@ n = String.to_integer(arg)
 #vet1 = Matrex.new(1, n, fn -> :rand.uniform() end)
 #vet2 = Matrex.new(1, n, fn -> :rand.uniform() end)
 
-vet1 = Hok.new_nx_from_function(1,n,{:f,64},fn -> :rand.uniform(1000) end )
-vet2 = Hok.new_nx_from_function(1,n,{:f,64},fn -> :rand.uniform(1000) end)
+#vet1 = Hok.new_nx_from_function(1,n,{:f,64},fn -> :rand.uniform(1000) end )
+#vet2 = Hok.new_nx_from_function(1,n,{:f,64},fn -> :rand.uniform(1000) end)
 
+vet1 = Hok.new_nx_from_function(1,n,{:f,32},fn -> 1 end )
+vet2 = Hok.new_nx_from_function(1,n,{:f,32},fn -> 1 end)
 
+#vet_1 = Hok.new_nx_from_function(1,n,{:s,32},fn -> 1 end )
+#vet_2 = Hok.new_nx_from_function(1,n,{:s,32},fn -> 1 end)
 
 prev = System.monotonic_time()
 
+Hok.include_rts [PMap2]
+Hok.set_default_type(:float)
+
+
 ref1= Hok.new_gnx(vet1)
 ref2 = Hok.new_gnx(vet2)
-ref3= Hok.new_gnx(1,n,{:f,64})
+ref3= Hok.new_gnx(1,n,{:f,32})
+
+#ref_1= Hok.new_gnx(vet_1)
+#ref_2= Hok.new_gnx(vet_2)
+#ref_3= Hok.new_gnx(1,n,{:s,32})
 
 
+#Hok.set_default_type(:float)
+PMap2.map2(ref1,ref2,ref3,n, &PMap2.saxpy/2)
 
+#Hok.set_default_type(:int)
+#PMap2.map2(ref_1,ref_2,ref_3,n,&PMap2.saxpy/2)
 
-PMap2.map2(ref1,ref2,ref3,n, Hok.lt(&PMap2.saxpy/2))
 #PMap2.map2(ref1,ref2,ref3,n, Hok.hok(fn (a,b) -> type a float; type b float; return 2*a+b end))
 #PMap2.map2(ref1,ref2,ref3,n, Hok.hok(fn (a,b) -> 2*a+b end))
 
 #Hok.synchronize()
 
-_result = Hok.get_gmatrex(ref3)
+_result = Hok.get_gnx(ref3)
+Hok.end_hok
 
 next = System.monotonic_time()
 IO.puts "Hok\t#{n}\t#{System.convert_time_unit(next-prev,:native,:millisecond)}"
 
 
 #IO.inspect result
+#IO.inspect result_
