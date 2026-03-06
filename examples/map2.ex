0667ddeebb102eb22e809fc7e70be8ba9eb53c74
Andre Rauber Du Bois <rauberdubois@gmail.com>
2026-03-06 07:06:16 -0300

    map2

---
+++ examples/map2.ex
@@ -0,0 +1,43 @@
+Hok.defmodule_rts Map do
+      deft map_2kernel (arr a) ~> (arr b) ~> (arr c) ~> integer ~> [a ~> b ~> c] 
+      defk map_2kernel(a1,a2,a3,size,f) do
+        id = blockIdx.x * blockDim.x + threadIdx.x
+        if(id < size) do
+          a3[id] = f(a1[id],a2[id])
+        end
+      end
+      def map2(t1,t2,func) do
+    
+        {l,c} = Hok.get_shape_gnx(t1)
+        type = Hok.get_type_gnx(t2)
+         size = l*c
+         result_gpu = Hok.new_gnx(l,c, type)
+    
+          threadsPerBlock = 256;
+          numberOfBlocks = div(size + threadsPerBlock - 1, threadsPerBlock)
+    
+          Hok.spawn_rts(&DP.map_2kernel/5,{numberOfBlocks,1,1},{threadsPerBlock,1,1},[t1,t2,result_gpu,size,func])
+    
+    
+          result_gpu
+      end
+end   
+
+
+Hok.include_rts [Map]
+Hok.set_default_type(%{default:  :float})
+
+n = 10000000
+
+#list = [Enum.to_list(1..n)]
+
+#mat1 = Matrex.new(list)
+#mat2 = Matrex.new(list)
+a = Hok.new_nx_from_function(1,n,{:f,32},fn -> 1 end )
+b = Hok.new_nx_from_function(1,n,{:s,32},fn -> 1 end )
+
+c = Map.map2(a,b, Hok.hok_rts fn x,y -> x+yend)
+
+result = Hok.get_gnx(c)
+
+IO.inspect result
